f4e66ab5a7cb5c80ec1af10f7c173cd0ae9e1fa1
Brijesh Kumar Yadav <bkumaryadav096@gmail.com>
2022-05-15 16:51:48 +0530

    todo-app

---
+++ build/precache-manifest.b9056ab8246d603e6f8260a3b0ba4ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c00ae3c79ad3237519f680fa27fd4a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d04782871e2c9c2f01b",
+    "url": "/static/css/main.cf8abd57.chunk.css"
+  },
+  {
+    "revision": "7fa26cb0299ab2c3b88b",
+    "url": "/static/js/2.c1ad851c.chunk.js"
+  },
+  {
+    "revision": "1ff3c45f60018cab500afb94c2685876",
+    "url": "/static/js/2.c1ad851c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d04782871e2c9c2f01b",
+    "url": "/static/js/main.a28e33f3.chunk.js"
+  },
+  {
+    "revision": "a213432152de1cb7b67d",
+    "url": "/static/js/runtime-main.ed736fd0.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,133 @@
+import React, {useState} from 'react';
+import './App.css';
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import _ from "lodash";
+import {v4} from "uuid";
+
+const item = {
+  id: v4(),
+  name: "Complete project before 15 may 2022"
+}
+
+const item2 = {
+  id: v4(),
+  name: "How are you sir?"
+}
+
+function App() {
+  const [text, setText] = useState("")
+  const [state, setState] = useState({
+    "todo": {
+      title: "Todo",
+      items: [item, item2]
+    },
+    "in-progress": {
+      title: "In Progress",
+      items: []
+    },
+    "done": {
+      title: "Completed",
+      items: []
+    }
+  })
+
+  const handleDragEnd = ({destination, source}) => {
+    if (!destination) {
+      return
+    }
+
+    if (destination.index === source.index && destination.droppableId === source.droppableId) {
+      return
+    }
+
+    // Creating a copy of item before removing it from state
+    const itemCopy = {...state[source.droppableId].items[source.index]}
+
+    setState(prev => {
+      prev = {...prev}
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1)
+
+
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
+
+      return prev
+    })
+  }
+
+  const addItem = () => {
+    setState(prev => {
+      return {
+        ...prev,
+        todo: {
+          title: "Todo",
+          items: [
+            {
+              id: v4(),
+              name: text
+            },
+            ...prev.todo.items
+          ]
+        }
+      }
+    })
+
+    setText("")
+  }
+
+  return (
+    <>
+    <h1>👋  Welcome To 2DO App</h1>
+    <div className="App">
+      <div className='addButton'>
+        <input className='inputAddItems' type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+        <button className='addItemButton' onClick={addItem}>Add</button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(state, (data, key) => {
+          return(
+            <div key={key} className={"column"}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return(
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {data.items.map((el, index) => {
+                        return(
+                          <Draggable key={el.id} index={index} draggableId={el.id}>
+                            {(provided, snapshot) => {
+                              console.log(snapshot)
+                              return(
+                                <div
+                                  className={`item ${snapshot.isDragging && "dragging"}`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  )
+                }}
+              </Droppable>
+            </div>
+          )
+        })}
+      </DragDropContext>
+    </div>
+    </>
+  );
+}
+
+export default App;
